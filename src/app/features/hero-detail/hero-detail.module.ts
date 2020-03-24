a34686b0f7ea5c30e2e1ad9d373d72617f0e54c0
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2020-03-24 17:50:25 -0500

    ng g c features/hero-detail/components/hero

--- src/app/features/hero-detail/hero-detail.module.ts
+++ src/app/features/hero-detail/hero-detail.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroDetailComponent } from './containers/hero-detail/hero-detail.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
-  declarations: [HeroDetailComponent],
+  declarations: [HeroDetailComponent, HeroComponent],
   imports: [
     CommonModule
   ]
