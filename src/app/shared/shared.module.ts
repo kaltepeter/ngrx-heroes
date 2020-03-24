39d614a1ac97fd893d39bf2efee508de15a458f4
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2020-03-24 17:45:26 -0500

    ng g c shared/components/messages

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MessagesComponent],
   imports: [
     CommonModule
   ]
