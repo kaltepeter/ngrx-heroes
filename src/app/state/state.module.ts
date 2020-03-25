5b51fd104504274eff37d6280ea39f9bf50c3cf5
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2020-03-25 09:19:07 -0500

    ng g @ngrx/schematics:store State --root --module state/state.module.ts --stateInterface AppReducer --statePath state --minimal

--- src/app/state/state.module.ts
+++ src/app/state/state.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forRoot({}, { 
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      }
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ]
 })
 export class StateModule { }
